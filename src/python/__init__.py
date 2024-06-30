fcc4e464c7d680ec66bc048b12fbd508337315a2
Wally Trenholm <wally416@gmail.com>
2024-06-30 08:23:22 -0400

    .

--- src/python/__init__.py
+++ src/python/__init__.py
@@ -1,4 +1,9 @@
 from k8spyobj import K8sBase
 
-class SupabaseTenant(K8sBase): 
+class SupabaseTenant(K8sCrdBase): 
+    """
+
+    the operator needs to watch for these objects and then deploy a supabase instance based on it
+
+    """
     pass
